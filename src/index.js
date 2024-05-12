6fd7fb643880f3e929003f863446180c523420f9
Matheus <szmatheussouza@gmail.com>
2024-05-12 00:22:21 -0300

    Iniciando projeto

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { createPool } from "mysql2/promise";
+import {config} from 'dotenv'
+config()
+
+const app = express();
+
+const pool = createPool({
+  host: process.env.MYSQLDB_HOST,
+  user: 'root',
+  password: process.env.MYSQLDB_ROOT_PASSWORD,
+  port: process.env.MYSQLDB_DOCKER_PORT,
+});
+
+pool.on("connection", () => console.log("DB Connected!"));
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result[0]);
+});
+
+app.listen(process.env.NODE_DOCKER_PORT || 3000);
+console.log("Server on port", process.env.NODE_DOCKER_PORT || 3000);
